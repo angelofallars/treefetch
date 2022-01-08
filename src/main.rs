143648e0cdf3f242a668874f19461c517c486074
Eden <62473844+EdenQwQ@users.noreply.github.com>
2022-01-08 17:17:33 +0800

    improved code
    Improved code with the help of rust-analyzer

--- src/main.rs
+++ src/main.rs
@@ -18,38 +18,10 @@ fn main() {
     reset = colors::reset,
 );
 
+    // Christmas tree if passed with -xmas argument
     let args: Vec<String> = env::args().collect();
     let mut is_christmas = false;
 
-
-    // bonsai tree if passed with -bonsai argument
-    if args.len() >= 2 && args[1] == "-bonsai" {
-    ascii_tree = format!(
-        "{green} {bold}             &               {reset}
-        {green}          && & &&             {reset}
-        {green}         &{yellow}_& & _/{green}&            {reset}
-        {yellow}{bold}           /~\\                {reset}
-        {green} &  & &{yellow}     /|                {reset}
-        {green} & {yellow}{bold}_&{reset}{green}&{yellow}   _\\_/|                {reset}
-        {green}&& {yellow}{bold}&{reset}{green}&&{yellow}_/    |\\                {reset}
-        {green}  &&{yellow}_|/{green}{bold} &{reset}{yellow}   \\~|{green}         && &   {reset}
-        {yellow}           \\//~\\{green}{bold}   &&{reset}{yellow} &&{green}&  {reset}
-        {yellow}            |/\\__/{green}& &{yellow}_/_{green}&&  {reset}
-        {yellow}              \\\\  /__{green}{bold}&{reset}{yellow}_{green}&&&    {reset}
-        {gray}        :{green}____{yellow}./~\\.{green}____{gray}:         {reset}
-        {gray}         \\___________/         {reset}
-        {gray}          (_)     (_)            {reset}
-        ",
-        gray = colors::gray,
-        green = colors::green,
-        yellow = colors::yellow,
-        reset = colors::reset,
-        bold = colors::bold,
-        );
-    }
-
-
-    // Christmas tree if passed with -xmas argument
     if args.len() >= 2 && args[1] == "-xmas" {
             ascii_tree = format!("{bright_yellow}{bold}      ★         {reset}
 {green}     /\\{red}{bold}o{green}\\       {reset}
@@ -75,45 +47,38 @@ fn main() {
 
     let mut data_list: Vec<String> = Vec::new();
 
-    match fields::get_user_host_name(is_christmas) {
-        Ok(value) => {
+    if let Ok(value) = fields::get_user_host_name(is_christmas) {
             data_list.push(value.0);
             data_list.push(value.1);
-        },
-        Err(_) => {}
     };
 
-    match fields::get_distro_name() {
-        Ok(value) => data_list.push(value),
-        Err(_) => {}
+
+    if let Ok(value) = fields::get_distro_name() {
+        data_list.push(value);
     };
 
     // Kernel name
 
-    match fields::get_kernel() {
-        Ok(value) => data_list.push(value),
-        Err(_) => {}
+    if let Ok(value) = fields::get_kernel() {
+        data_list.push(value);
     };
 
     // Shell
 
-    match fields::get_shell() {
-        Ok(value) => data_list.push(value),
-        Err(_) => {}
+    if let Ok(value) = fields::get_shell() {
+        data_list.push(value);
     };
 
     // Uptime
 
-    match fields::get_uptime() {
-        Ok(value) => data_list.push(value),
-        Err(_) => {}
+    if let Ok(value) = fields::get_uptime() {
+        data_list.push(value);
     };
 
     // Memory
 
-    match fields::get_memory() {
-        Ok(value) => data_list.push(value),
-        Err(_) => {}
+    if let Ok(value) = fields::get_memory() {
+        data_list.push(value);
     };
 
     print_left_to_right(ascii_tree, data_list, is_christmas);
@@ -145,7 +110,7 @@ fn print_left_to_right(left: Vec<String>, right: Vec<String>,
         }
 
         // Print a newline
-        println!("");
+        println!();
     }
 }
 
